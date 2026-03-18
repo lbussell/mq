15de21b9364b49b56bc6bb2ce72216c6be1e770b
Logan Bussell <github@loganbussell.com>
2026-03-17 21:45:47 -0700

    Update demo script

--- demo.sh
+++ demo.sh
@@ -5,14 +5,6 @@ if ! command -v gh &>/dev/null; then
         exit 1
 fi
 
-if ! command -v glow &>/dev/null; then
-        echo "Error: 'glow' is not installed (used to prettify Markdown output). Install it from https://github.com/charmbracelet/glow" >&2
-        exit 1
-fi
-
-gh repo view dotnet/runtime \
-        --json nameWithOwner,url,licenseInfo,issues,pullRequests,primaryLanguage,updatedAt,stargazerCount,watchers,latestRelease |
-        dotnet run --project src/mq -- --title nameWithOwner |
-        glow -
-
-gh pr ls --repo dotnet/runtime --limit 5 --json title,id,author | dotnet run --project src/mq -- --table "."
+gh pr view --repo dotnet/docker-tools 2030 --json title,id,author,statusCheckRollup |
+        jq '.statusCheckRollup |= [.[] | {name, conclusion, status}]' |
+        just run --title title --table statusCheckRollup
